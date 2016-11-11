37a1d09c3276a0f1c68482ef4a2823070f8af643
Stephen Gutknecht <stephen.gutknecht@gmail.com>
2016-11-10 21:02:03 -0500

    Poorly coded Timer C to Java and back for Glk. zracer.z5 opening screen at least slowly moves.

---
+++ app/src/main/java/org/andglkmod/glk/TimerEvent.java
@@ -0,0 +1,11 @@
+package org.andglkmod.glk;
+
+/**
+ * Created by adminsag on 11/10/16.
+ */
+
+public class TimerEvent extends Event {
+    public TimerEvent(Window w) {
+        super(w);
+    }
+}
